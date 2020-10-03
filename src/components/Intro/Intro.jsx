48443583c5f36be5c455185ebcc7cb1a3c7ca8e3
Oliver Levy <oliver.levy.ol@gmail.com>
2020-10-03 00:55:14 -0400

    added an intro component

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Intro() {
+  return (
+    <div>
+      <h1>This is the intro component</h1>
+      <h2>
+        Hello, <br />
+        My name is Oli
+      </h2>
+      <p>
+        I’m a full-stack web developer who <br /> designed furniture & lighting
+        for Gabriel-Scott,
+        <br /> repaired bikes for many years,
+        <br /> loves his three beautiful dogs,
+        <br /> Bill, Franklin and Chester.
+      </p>
+    </div>
+  );
+}
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Intro from "../Intro/Intro";
 import ProjectCards from "../ProjectCards/ProjectCards";
 
 export default function Landing() {
   return (
     <div>
       <h1>This is my landing page</h1>
+      <Intro/>
       <ProjectCards />
     </div>
   );
