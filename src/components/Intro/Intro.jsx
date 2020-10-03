1c8af8b02104aeb1c67433fb5bd749b49b3ab491
Oliver Levy <oliver.levy.ol@gmail.com>
2020-10-03 01:37:40 -0400

    playing around with the idea of sending styling through graphQl to give clients the ability to style their own components such as buttons etc

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,20 +1,52 @@
 import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 export default function Intro() {
   return (
     <div>
-      <h1>This is the intro component</h1>
-      <h2>
-        Hello, <br />
-        My name is Oli
-      </h2>
-      <p>
-        I’m a full-stack web developer who <br /> designed furniture & lighting
-        for Gabriel-Scott,
-        <br /> repaired bikes for many years,
-        <br /> loves his three beautiful dogs,
-        <br /> Bill, Franklin and Chester.
-      </p>
+      <Query
+        query={gql`
+          {
+            stylin(where: { id: "ckft7yjjc1f5q017998sw0we6" }) {
+              border
+              borderRadius
+              height
+              width
+              background {
+                hex
+              }
+            }
+          }
+        `}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return <h1>loading...</h1>;
+          if (error) return <h1>error :(</h1>;
+          console.log(data);
+          const btn = data.stylin;
+
+          return (
+            <div>
+              <h1>This is the intro component</h1>
+              <h2>
+                Hello, <br />
+                My name is Oli
+              </h2>
+              <p>
+                I’m a full-stack web developer who
+                <br /> designed furniture & lighting for Gabriel-Scott,
+                <br /> repaired bikes for many years,
+                <br /> loves his three beautiful dogs,
+                <br /> Bill, Franklin and Chester.
+              </p>
+              <button style={btn}>
+                nothing
+              </button>
+            </div>
+          );
+        }}
+      </Query>
     </div>
   );
 }
